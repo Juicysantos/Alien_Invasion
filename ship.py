d88bc5a3de5798eb242544b6875826a73cda1959
juicy_santosj <dulcidoniods@gmail.com>
2024-08-08 12:21:10 +0200

    Left Movement added and Screen boundaries

--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
 
@@ -17,14 +18,27 @@ class Ship:
         #Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        #Store a float for the ship's exact horizontal postion. By defactul rect x is integer
+        self.x = float(self.rect.x)
+
         #Movement Flag; start with a ship that's not moving
         self.moving_right = False
+        self.moving_left = False
     
 
     def update(self):
         """Update the ships position based on the movement flag"""
-        if self.moving_right:
-            self.rect.x += 1
+        #update the ship's x value not the rect like before
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            #self.rect.x += 1
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            #self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        
+        #Update rect object from self.x
+        self.rect.x = self.x
     
     def blitme(self):
         """Draw the ship at it's current location"""
